789ba13cc5558fdd70d18635ef0455d991a68b55
om <omejjigiri99@gmail.com>
2024-07-20 12:33:09 +0530

    cmmit

--- loop.sh
+++ loop.sh
@@ -1,7 +1,12 @@
 
-a=10
-while [ "$a" -gt 0 ]; do
-  echo hello om 
-  a=$(($a-1))
-  sleep 1
-done
+# a=10
+# while [ "$a" -gt 0 ]; do
+#   echo hello om 
+#   a=$(($a-1))
+#   sleep 1
+# done
+
+for fruits in apple banana orange ; do
+   echo fruit name - $fruit
+   sleep 1
+done   
